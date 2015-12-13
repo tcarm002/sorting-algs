1c53f44bc31579e92748814fd67896f8291a0c75
tcarm002 <tcarm002@fiu.edu>
2015-12-13 14:05:54 -0500

    Added comments

--- SortingAlgorithms/src/sortingalgorithms/Quicksort.java
+++ SortingAlgorithms/src/sortingalgorithms/Quicksort.java
@@ -1,16 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sortingalgorithms;
 
 /**
- *
+ * Implements the Quicksort algorithm, using the last element as pivot.
  * @author sol
  */
 public class Quicksort {
 
+    /**
+     * Recursively calls itself on smaller and smaller partitions of the array
+     * until an array of size zero is returned.
+     * 
+     * @param arr The unsorted array.
+     * @param low The first index of the array to be sorted.
+     * @param high The last index of the array to be sorted.
+     * @return arr: The sorted array.
+     */
     public static int [] quicksort(int[] arr, int low, int high) {
         int p;
         if (low < high) {
@@ -21,6 +25,15 @@ public class Quicksort {
         return arr;
     }
 
+    /**
+     * Sorts the array, using a pivot, placing smaller items to left of it
+     * and larger items to the right.
+     * 
+     * @param arr The array to sort.
+     * @param low The smallest index of the array.
+     * @param high The largest index of the array.
+     * @return i : The current index location of the pivot.
+     */
     public static int partition(int[] arr, int low, int high) {
         int pivot = arr[high];
         int i = low;
